9e5ede6d217ea72ba5c0c36d43de85d2abb3c2d0
ServerusM <sergiomamani2014@gmail.com>
2024-10-07 09:39:15 -0400

    Changes board test

--- API-Aut-Trello/trello-api/src/test/java/com/trello/BoardTest.java
+++ API-Aut-Trello/trello-api/src/test/java/com/trello/BoardTest.java
@@ -6,22 +6,28 @@ package com.trello;
 
 
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
 
 
 public class BoardTest {
+    private static RequestSpecification requestSpec;
+    @BeforeMethod
+    public void setUp(){
+        requestSpec = new RequestSpecBuilder().setBaseUri("https://api.trello.com/1").build();
+
+    }
     @Test
     public void testCreateBoard(){
-        String endpoint = "https://api.trello.com/1/boards/";
         String apiKey = "";
         String apiToken = "";
-        String boarName = "Sergio test Api4";
-//        String endpoint = baseUrl+"?name="+boarName+"&key="+apiKey+"&token="+apiToken;
+        String boarName = "Sergio test Api Resquest-1 ";
 
-//        var response = RestAssured.given().log().all().when().post(endpoint);
         var headers = new HashMap<String, String>();
         headers.put("Content-Type", "application/son");
 
@@ -31,10 +37,40 @@ public class BoardTest {
         queryParams.put("name", boarName);
 
         //Act
-        var response = RestAssured.given().log().all().when()
-                        .headers(headers)
-                        .queryParams(queryParams)
-                                .post(endpoint);
+        var response = RestAssured.given()
+                .spec(requestSpec)
+                .log().all().when()
+                .headers(headers)
+                .queryParams(queryParams)
+                .post("/boards/");
+        //Assert
+        Assert.assertEquals(response.statusCode(),200);
+    }
+
+    @Test
+    public void testCreateBoardReqSpec(){
+        //Arrange
+        String apiKey = "";
+        String apiToken = "";
+        String boarName = "Sergio test Api Resquest-1-2 ";
+
+        var headers = new HashMap<String, String>();
+        headers.put("Content-Type", "application/son");
+
+        var queryParams = new HashMap<String, String>();
+        queryParams.put("key", apiKey);
+        queryParams.put("token", apiToken);
+        queryParams.put("name", boarName);
+
+        //Act
+        var response = RestAssured.given()
+                .spec(requestSpec)
+                .log().all().when()
+                .headers(headers)
+                .queryParams(queryParams)
+                .post("/boards/");
+
+        System.out.println(response.getBody().asPrettyString());
         //Assert
         Assert.assertEquals(response.statusCode(),200);
     }
