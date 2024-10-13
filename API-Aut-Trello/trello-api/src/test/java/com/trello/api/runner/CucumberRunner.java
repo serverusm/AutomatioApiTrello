adfd4fd7ca113939259f5dfef7be46a7db3f2816
ServerusM <sergiomamani2014@gmail.com>
2024-10-13 17:14:53 -0400

    Added Allure cucumber report

--- API-Aut-Trello/trello-api/src/test/java/com/trello/api/runner/CucumberRunner.java
+++ API-Aut-Trello/trello-api/src/test/java/com/trello/api/runner/CucumberRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.testng.CucumberOptions;
  */
 @CucumberOptions(
         plugin = {"pretty", "json:reports/cucumber.json", "junit:report/cucumber.xml",
-        "html:report/cucumber-html-report.html"},
+        "html:report/cucumber-html-report.html", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
         features = {"src/test/resources/features"},
 //        trello-api\src\test\resources\features
         glue = {"com.trello.api"}
